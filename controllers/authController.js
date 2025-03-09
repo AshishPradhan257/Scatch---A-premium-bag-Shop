41e3527f78205f53487e223747c983961e272021
AshishPradhan257 <ashish257pradhan@gmail.com>
2025-03-09 06:09:58 +0530

    creating products

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,16 +37,24 @@ module.exports.loginUser = async function(req, res){
     let {email, password} = req.body;
 
     let user = await userModel.findOne({email: email});
-    if(!user) return res.send("Email or Password incorrect");
+    if(!user) {
+        req.flash("error", "Email or Password incorrect");
+        return res.redirect("/");
+    }
 
     bcrypt.compare(password, user.password, function(err, result){
         if(result){
             let token = generateToken(user);
             res.cookie("token", token);
-            res.send("you can login");
+            res.redirect("/shop");
         }
         else{
             return res.send("Email or Password incorrect");
         }
     })
 }
+
+module.exports.logout = function(req, res){
+    res.cookie("token", "");
+    res.redirect("/");
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const productModel = require("../models/product-model"); // Import product model
+const isLoggedin = require("../middlewares/isloggedin");
 
 router.get("/", (req, res) => {
-    res.render("index", { error: "" }); // Ensure error is always defined
+    let error = req.flash("error");
+    res.render("index", { error }); // Ensure error is always defined
+});
+
+
+
+router.get("/shop", isLoggedin, async function(req, res) {
+    try {
+        let products = await productModel.find(); // Fetch all products from DB
+        res.render("shop", { products }); // ✅ Pass products to EJS template
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Error fetching products");
+    }
+});
+
+
+router.get("/logout", isLoggedin, function(req, res){
+    res.render("shop");
 });
 
 module.exports = router;
